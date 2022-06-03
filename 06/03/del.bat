b6568e81d538f107dacb122c2443b6764a51a829
断固非難 <4385327+wotupset@users.noreply.github.com>
2022-06-03 23:03:20 +0800

    Add files via upload

---
+++ 06/03/del.bat
@@ -0,0 +1,14 @@
+pause
+del /q list
+copy list.bat .\list\list.bat
+del /q list2
+copy list.bat .\list2\list.bat
+
+echo 123 >-.txt
+
+
+exit
+pause
+
+del /q list2021
+copy list.bat .\list2021\list.bat
